2f8a6438eca15390b6226daf58372f3f9eb07994
IT-Gaigo-WebProgrammingCourse <htun@osf.ac.jp>
2023-04-05 11:43:08 +0900

    Add pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'MySelf'
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/',
