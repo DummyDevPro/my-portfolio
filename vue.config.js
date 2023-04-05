71eaf05e37fbe92419dd3df27fc5c05360abf58f
IT-Gaigo-WebProgrammingCourse <htun@osf.ac.jp>
2023-04-05 12:13:54 +0900

    Change config

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // pages: {
-  //   index: {
-  //     entry: 'src/main.js',
-  //     template: 'public/index.html',
-  //     filename: 'index.html',
-  //     title: 'MySelf'
-  //   }
-  // },
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/' + process.env.CI_PROJECT_NAME + '/'
-  //   : '/',
-  // transpileDependencies: true
-  outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/',
+  transpileDependencies: true
+  // outputDir: 'docs',
+  // assetsDir: './',
+  // publicPath: './',
 })
