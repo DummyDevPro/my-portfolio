383bd5612cfec0f9f45bd756d29cf3fccd6348f8
IT-Gaigo-WebProgrammingCourse <htun@osf.ac.jp>
2023-04-05 17:01:39 +0900

    Add Scroll Behavior

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {    
+  {
     path: '/',
     name: 'home',
     component: HomeView
   }
 ]
 
+const scrollBehavior = (to, from, savedPosition) => {
+  return savedPosition || { top: 0, left: 0 }
+}
+
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior
 })
 
 export default router
